32addc82337f0655637aa75e38b3d3117c930390
BanLowi <lowi40k@gmail.com>
2025-11-12 12:10:14 +0100

    fetching inside actors

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,24 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 function App() {
+
+  const [actors, setActors] = useState([])
+
+  function fetchCharacters() {
+
+    axios.get("https://lanciweb.github.io/demo/api/actors/")
+      .then(response => setActors(response.data))
+
+  }
+
+  useEffect(fetchCharacters, [])
+
+  console.log(actors);
+
+
+
+
 
   return (
     <>
