56ca8d775e92e51db7adb20a7ec114654f6178f3
BanLowi <lowi40k@gmail.com>
2025-11-12 12:26:24 +0100

    actor list printed on page

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,31 @@ function App() {
 
     axios.get("https://lanciweb.github.io/demo/api/actors/")
       .then(response => setActors(response.data))
-
   }
 
   useEffect(fetchCharacters, [])
-
-  console.log(actors);
-
-
-
-
+  console.log(actors); // Stampo in console l'endpoint
 
   return (
     <>
+
+      <header>
+        <h1>Cast Fetching</h1>
+      </header>
+
+      <main>
+
+        <div className="container">
+          <ul className="list-group list-group-numbered">
+            {
+              actors.map(actor => (
+                <li className="list-group-item">{actor.name}</li>
+              ))
+            }
+          </ul>
+        </div>
+
+      </main>
 
     </>
   )
