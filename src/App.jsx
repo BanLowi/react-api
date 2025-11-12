58ffc5cbcc069fd7174276c95551757da079f391
BanLowi <lowi40k@gmail.com>
2025-11-12 12:44:09 +0100

    actresses list printed on page

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,26 @@ import { useEffect, useState } from "react"
 function App() {
 
   const [actors, setActors] = useState([])
+  const [actresses, setActresses] = useState([])
 
-  function fetchCharacters() {
+  function fetchActors() {
 
     axios.get("https://lanciweb.github.io/demo/api/actors/")
       .then(response => setActors(response.data))
   }
 
-  useEffect(fetchCharacters, [])
+  function fetchActresses() {
+
+    axios.get("https://lanciweb.github.io/demo/api/actresses/")
+      .then(response => setActresses(response.data))
+  }
+
+  useEffect(fetchActors, [])
   console.log(actors); // Stampo in console l'endpoint
+
+  useEffect(fetchActresses, [])
+  console.log(actresses);
+
 
   return (
     <>
@@ -24,13 +35,27 @@ function App() {
       <main>
 
         <div className="container">
-          <ul className="list-group list-group-numbered">
-            {
-              actors.map(actor => (
-                <li className="list-group-item">{actor.name}</li>
-              ))
-            }
-          </ul>
+          <div>
+            <h2>ACTORS</h2>
+            <ul className="list-group list-group-numbered">
+              {
+                actors.map(actor => (
+                  <li key={actor.id} className="list-group-item">{actor.name}</li>
+                ))
+              }
+            </ul>
+          </div>
+
+          <div>
+            <h2>ACTRESSES</h2>
+            <ul className="list-group list-group-numbered">
+              {
+                actors.map(actor => (
+                  <li key={actor.id} className="list-group-item">{actor.name}</li>
+                ))
+              }
+            </ul>
+          </div>
         </div>
 
       </main>
